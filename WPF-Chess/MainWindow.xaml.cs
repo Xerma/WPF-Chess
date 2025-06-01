35b4d1e504ccc74a6a00f66a94748dc97601a069
cheave2 <cheave2@wgu.edu>
2025-06-01 18:58:43 -0500

    add game manager

--- WPF-Chess/MainWindow.xaml.cs
+++ WPF-Chess/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF_Chess.Classes.Managers;
 
 namespace WPF_Chess
 {
@@ -27,6 +28,8 @@ namespace WPF_Chess
             // load: JSON
 
             InitializeComponent();
+
+            GameManager gameManager = new GameManager();
         }
     }
 }
