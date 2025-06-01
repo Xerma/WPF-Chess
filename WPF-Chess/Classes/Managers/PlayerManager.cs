60244b72f2524635daf6360de7eba39cfac16550
cheave2 <cheave2@wgu.edu>
2025-06-01 18:59:11 -0500

    add PlayerManager

---
+++ WPF-Chess/Classes/Managers/PlayerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_Chess.Classes.Pieces;
+
+namespace WPF_Chess.Classes.Managers
+{
+    internal class PlayerManager
+    {
+        private Piece[,] _piecesSet;
+        private Piece[] _pawnSet;
+        private Piece[] _rearSet;
+
+        public PlayerManager()
+        {
+
+        }
+
+        public Piece[,] AddHumanPlayer(Owner owner)
+        {
+            
+        }
+
+        public void AddBotPlayer()
+        {
+
+        }
+    }
+}
