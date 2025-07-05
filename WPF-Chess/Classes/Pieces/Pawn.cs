9996e7477ea2ab7ed8f65e77a317ba62ec33e518
cheave2 <cheave2@wgu.edu>
2025-07-05 15:31:50 -0500

    Switch to Vector2D for movement and attacking. Use switch instead of if/else in ctor

--- WPF-Chess/Classes/Pieces/Pawn.cs
+++ WPF-Chess/Classes/Pieces/Pawn.cs
@@ -1,29 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
+using WPF_Chess.Structs;
 
 namespace WPF_Chess.Classes.Pieces
 {
     internal class Pawn : Piece
     {
+        private readonly Vector2D[] _pawnMoves;
+        public override Vector2D[] MovementVectors => _pawnMoves;
+
+        private readonly Vector2D[] _pawnAttacks;
+        public override Vector2D[] AttackVectors => _pawnAttacks;
+
         public Pawn(Owner owner, int number) : base(PieceType.Pawn, owner, number)
         {
-            AttackRangeDiagonal = 1;
-
-            if (owner == Owner.P2) // top
+            switch (owner)
             {
-                MoveRangeDown = 1;
-                CanAttackDown = true;
+                case Owner.P2: // TOP
+                    _pawnMoves = [new(0, -1)];
+                    _pawnAttacks = [new(-1, -1), new(1, -1)];
+                    break;
+                case Owner.P1: // BOTTOM
+                    _pawnMoves = [new(0, 1)];
+                    _pawnAttacks = [new(-1, 1), new(1, 1)];
+                    break;
+                default:
+                    throw new ArgumentException("Unknown owner", nameof(owner));
             }
-            if (owner == Owner.P1) // bottom
-            {
-                MoveRangeUp = 1;
-                CanAttackUp = true;
-            }
-            
         }
     }
 }
