4d93f52efa8db516d645db4fabcefd3f09dac921
cheave2 <cheave2@wgu.edu>
2025-07-05 15:33:09 -0500

    Remove unnecessary comments

--- WPF-Chess/MainWindow.xaml.cs
+++ WPF-Chess/MainWindow.xaml.cs
@@ -15,18 +15,13 @@ namespace WPF_Chess
     //Rows == 1 - 8
     //Columns == A - H
 
-    //row# [0, 1, 2, 3, 4, 5, 6, 7]
-    //col   A, B, C, D, E, F, G, H
+    //row  [0, 1, 2, 3, 4, 5, 6, 7]
+    //col  [A, B, C, D, E, F, G, H]
 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            // open to empty board
-            // player chooses new game or load game
-            // new: select pvp local or pvp connect (network)
-            // load: JSON
-
             InitializeComponent();
 
             GameManager gameManager = new GameManager();
