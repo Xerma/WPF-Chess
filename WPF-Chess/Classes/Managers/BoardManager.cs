81506c7e54ed17087eda75475391f9299209496f
cheave2 <cheave2@wgu.edu>
2025-07-26 18:49:34 -0500

    Moved pawn building to constructor for better access Added placehold methods (69 - 107)

--- WPF-Chess/Classes/Managers/BoardManager.cs
+++ WPF-Chess/Classes/Managers/BoardManager.cs
@@ -14,12 +14,50 @@ namespace WPF_Chess.Classes.Managers
 {
     internal class BoardManager
     {
-        //board controller detects piece type and changes tile image and owner overlay (rectangle in user control)
-        private MainWindow _mainWindow;
+        private readonly MainWindow _mainWindow;
+
+        // PLAYER 2
+        private readonly PawnControl P2_Pawn1;
+        private readonly PawnControl P2_Pawn2;
+        private readonly PawnControl P2_Pawn3;
+        private readonly PawnControl P2_Pawn4;
+        private readonly PawnControl P2_Pawn5;
+        private readonly PawnControl P2_Pawn6;
+        private readonly PawnControl P2_Pawn7;
+        private readonly PawnControl P2_Pawn8;
+
+        // PLAYER 1
+        private readonly PawnControl P1_Pawn1;
+        private readonly PawnControl P1_Pawn2;
+        private readonly PawnControl P1_Pawn3;
+        private readonly PawnControl P1_Pawn4;
+        private readonly PawnControl P1_Pawn5;
+        private readonly PawnControl P1_Pawn6;
+        private readonly PawnControl P1_Pawn7;
+        private readonly PawnControl P1_Pawn8;
 
         public BoardManager(MainWindow window)
         {
             _mainWindow = window;
+
+            P2_Pawn1 = new PawnControl(Owner.P2, _mainWindow.A7.Name);
+            P2_Pawn2 = new PawnControl(Owner.P2, _mainWindow.B7.Name);
+            P2_Pawn3 = new PawnControl(Owner.P2, _mainWindow.C7.Name);
+            P2_Pawn4 = new PawnControl(Owner.P2, _mainWindow.D7.Name);
+            P2_Pawn5 = new PawnControl(Owner.P2, _mainWindow.E7.Name);
+            P2_Pawn6 = new PawnControl(Owner.P2, _mainWindow.F7.Name);
+            P2_Pawn7 = new PawnControl(Owner.P2, _mainWindow.G7.Name);
+            P2_Pawn8 = new PawnControl(Owner.P2, _mainWindow.H7.Name);
+
+            P1_Pawn1 = new PawnControl(Owner.P1, _mainWindow.A2.Name);
+            P1_Pawn2 = new PawnControl(Owner.P1, _mainWindow.B2.Name);
+            P1_Pawn3 = new PawnControl(Owner.P1, _mainWindow.C2.Name);
+            P1_Pawn4 = new PawnControl(Owner.P1, _mainWindow.D2.Name);
+            P1_Pawn5 = new PawnControl(Owner.P1, _mainWindow.E2.Name);
+            P1_Pawn6 = new PawnControl(Owner.P1, _mainWindow.F2.Name);
+            P1_Pawn7 = new PawnControl(Owner.P1, _mainWindow.G2.Name);
+            P1_Pawn8 = new PawnControl(Owner.P1, _mainWindow.H2.Name);
+
             NewBoard();
         }
 
@@ -28,25 +66,64 @@ namespace WPF_Chess.Classes.Managers
             SetPawns();
         }
 
+        public void HighlightMovements()
+        {
+
+        }
+
+        public void HighlightAttacks()
+        {
+
+        }
+
+        public void MovePieceTo(UserControl control, string newLocation)
+        {
+
+        }
+
+        public void AddPieceAt(UserControl control, string location)
+        {
+
+        }
+
+        public void RemovePieceAt(UserControl control)
+        {
+            //_mainWindow.FindName(control.posi)
+        }
+
+        public bool PieceExistsAt(string location)
+        {
+            return false;
+        }
+
+        public string ApplyVector(string boardLocation, Vector2D moveVector)
+        {
+            Vector2D boardLocationToVector2D = Vector2D.ToBoardGrid(boardLocation);
+            int appliedX = boardLocationToVector2D.X + moveVector.X;
+            int appliedY = boardLocationToVector2D.Y + moveVector.Y;
+            Vector2D newVector = new Vector2D(appliedX, appliedY);
+            return newVector.ToBoardString();
+        }
+
         private void SetPawns()
         {
-            _mainWindow.A7.Child = new PawnControl(Owner.P2, _mainWindow.A7.Name);
-            _mainWindow.B7.Child = new PawnControl(Owner.P2, _mainWindow.B7.Name);
-            _mainWindow.C7.Child = new PawnControl(Owner.P2, _mainWindow.C7.Name);
-            _mainWindow.D7.Child = new PawnControl(Owner.P2, _mainWindow.D7.Name);
-            _mainWindow.E7.Child = new PawnControl(Owner.P2, _mainWindow.E7.Name);
-            _mainWindow.F7.Child = new PawnControl(Owner.P2, _mainWindow.F7.Name);
-            _mainWindow.G7.Child = new PawnControl(Owner.P2, _mainWindow.G7.Name);
-            _mainWindow.H7.Child = new PawnControl(Owner.P2, _mainWindow.H7.Name);
+            _mainWindow.A7.Child = P2_Pawn1;
+            _mainWindow.B7.Child = P2_Pawn2;
+            _mainWindow.C7.Child = P2_Pawn3;
+            _mainWindow.D7.Child = P2_Pawn4;
+            _mainWindow.E7.Child = P2_Pawn5;
+            _mainWindow.F7.Child = P2_Pawn6;
+            _mainWindow.G7.Child = P2_Pawn7;
+            _mainWindow.H7.Child = P2_Pawn8;
 
-            _mainWindow.A2.Child = new PawnControl(Owner.P1, _mainWindow.A2.Name);
-            _mainWindow.B2.Child = new PawnControl(Owner.P1, _mainWindow.B2.Name);
-            _mainWindow.C2.Child = new PawnControl(Owner.P1, _mainWindow.C2.Name);
-            _mainWindow.D2.Child = new PawnControl(Owner.P1, _mainWindow.D2.Name);
-            _mainWindow.E2.Child = new PawnControl(Owner.P1, _mainWindow.E2.Name);
-            _mainWindow.F2.Child = new PawnControl(Owner.P1, _mainWindow.F2.Name);
-            _mainWindow.G2.Child = new PawnControl(Owner.P1, _mainWindow.G2.Name);
-            _mainWindow.H2.Child = new PawnControl(Owner.P1, _mainWindow.H2.Name);
+            _mainWindow.A2.Child = P1_Pawn1;
+            _mainWindow.B2.Child = P1_Pawn2;
+            _mainWindow.C2.Child = P1_Pawn3;
+            _mainWindow.D2.Child = P1_Pawn4;
+            _mainWindow.E2.Child = P1_Pawn5;
+            _mainWindow.F2.Child = P1_Pawn6;
+            _mainWindow.G2.Child = P1_Pawn7;
+            _mainWindow.H2.Child = P1_Pawn8;
         }
     }
 }
