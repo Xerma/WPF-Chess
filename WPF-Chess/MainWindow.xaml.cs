5e6a08fbe8109101bc7050fbe4d7bccb3a3e19ce
cheave2 <cheave2@wgu.edu>
2025-07-26 19:19:08 -0500

    Added BoardManager instantiation

--- WPF-Chess/MainWindow.xaml.cs
+++ WPF-Chess/MainWindow.xaml.cs
@@ -12,12 +12,6 @@ using WPF_Chess.Classes.Managers;
 
 namespace WPF_Chess
 {
-    //Rows == 1 - 8
-    //Columns == A - H
-
-    //row  [0, 1, 2, 3, 4, 5, 6, 7]
-    //col  [A, B, C, D, E, F, G, H]
-
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -25,6 +19,7 @@ namespace WPF_Chess
             InitializeComponent();
 
             GameManager gameManager = new(this);
+            BoardManager boardManager = new(this);
         }
     }
 }
